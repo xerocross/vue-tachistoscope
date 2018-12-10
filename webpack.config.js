d6d20f906ac6b71bcefdc5d5f115f7d2f9b1b32b
xerocross <adam.cross.inc@gmail.com>
2018-12-10 16:14:56 -0500

    update packaging

--- webpack.config.js
+++ webpack.config.js
@@ -2,18 +2,14 @@ const path = require('path')
 const VueLoaderPlugin = require('vue-loader/lib/plugin')
 const webpack = require('webpack')
 
-module.exports = {
+module.exports = [{
     entry : {
         'demoindex' : './src/demoindex.js',
-        'index' : './src/index.js'
     },
     output : {
         path : path.resolve(__dirname, './dist'),
         publicPath : '/dist',
-        filename : '[name].js',
-        library : 'VueTachistoscope',
-        libraryTarget : 'umd',
-        umdNamedDefine : true
+        filename : '[name].js'
     },
     module : {
         rules : [
@@ -60,4 +56,59 @@ module.exports = {
         port : 9000,
         watchContentBase : true
     }
-}
+},
+{
+    entry : {
+        'index' : './src/index.js'
+    },
+    output : {
+        path : path.resolve(__dirname, './dist'),
+        publicPath : '/dist',
+        filename : '[name].js',
+        library : 'VueTachistoscope',
+        libraryTarget : 'umd',
+        umdNamedDefine : true
+    },
+    externals : {
+        "vue" : "Vue"
+    },
+    module : {
+        rules : [
+            {
+                test : /\.vue$/,
+                exclude : /node_modules/,
+                loader : 'vue-loader'
+            },
+            {
+                test : /\.js$/,
+                exclude : /node_modules/,
+                use : {
+                    loader : 'babel-loader'
+                }
+            },
+            {
+                test : /\.scss$/,
+                use : [
+                    {
+                        loader : 'style-loader' // creates style nodes from JS strings
+                    },
+                    {
+                        loader : 'css-loader' // translates CSS into CommonJS
+                    },
+                    {
+                        loader : 'sass-loader' // compiles Sass to CSS
+                    }
+                ]
+            }
+        ]
+    },
+    optimization : {
+        minimize : true
+    },
+    plugins : [
+        new webpack.DefinePlugin({
+            'NODE_ENV' : JSON.stringify(process.env.NODE_ENV)
+        }),
+        new VueLoaderPlugin()
+    ],
+}]
